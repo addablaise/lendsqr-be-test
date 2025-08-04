a5e23d2428cae6c4e74c2d35ac9de1527b81b18a
Blaise Adda <addablaise@gmail.com>
2025-08-04 09:25:29 +0000

    Knexfile update

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
   development: {
     client: 'mysql2',
     connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASSWORD,
-      database : process.env.DB_NAME,
+      host     : 'u0zbt18wwjva9e0v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      user     : 'k5cur9pqmob9r1d0',
+      password : 'ufwpkw9renpxvlx5',
+      database : 's4kx6f0ln1omwxys',
     },
     migrations: { directory: './migrations' }
   },
